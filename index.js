a515d9734e157c3c86e48d7d1a3d85488468355a
joseigor <joseigorcfm@gmail.com>
2019-07-28 23:41:38 -0300

    first simple example that mocks a temperature device and sends random tmeperature in the range 0-30 to the topic 'temperature-sensor'

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-async function publishMessage(topicName, data) {
+require("dotenv").config();
+
+async function publishMessage(topicName = "temperature-sensor", temp = null) {
   // [START pubsub_publish]
   // [START pubsub_quickstart_publisher]
   // Imports the Google Cloud client library
-  const {PubSub} = require('@google-cloud/pubsub');
+  const { PubSub } = require("@google-cloud/pubsub");
 
   // Creates a client
   const pubsub = new PubSub();
@@ -10,17 +12,21 @@ async function publishMessage(topicName, data) {
   /**
    * TODO(developer): Uncomment the following lines to run the sample.
    */
-  // const topicName = 'temperature-sensor';
-  // const data = JSON.stringify({ temp: '10deg' });
+  // generates random numbers in the range from 0 to 30
+  const randomTemp = temp ? temp : Number(Math.random() * 30).toFixed(2);
+  const data = JSON.stringify({ temperature: `${randomTemp}` });
 
   // Publishes the message as a string, e.g. "Hello, world!" or JSON.stringify(someObject)
   const dataBuffer = Buffer.from(data);
 
   const messageId = await pubsub.topic(topicName).publish(dataBuffer);
   console.log(`Message ${messageId} published.`);
+  console.log(data);
 
   // [END pubsub_publish]
   // [END pubsub_quickstart_publisher]
 }
 
-publishMessage('temperature-sensor', { temp: '10deg' }).catch(erro =>{console.log('o que foi que houve : ' +erro)});
+publishMessage("temperature-sensor").catch(erro => {
+  console.log("Erro captured:" + erro);
+});
